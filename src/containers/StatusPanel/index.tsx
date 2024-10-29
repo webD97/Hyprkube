9c70ebabc9712908f887f3e57a9b41b30292ad01
Christian Danscheid <webD97@users.noreply.github.com>
2024-10-29 22:48:04 +0100

    fix: remove obsolete field from status bar

--- src/containers/StatusPanel/index.tsx
+++ src/containers/StatusPanel/index.tsx
@@ -2,25 +2,21 @@ import { listen } from "@tauri-apps/api/event";
 import { useState, useEffect } from "react";
 
 type JoinHandleStoreStatsPayload = {
-    channels: number,
     handles: number
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 const StatusPanel: React.FC<{}> = () => {
-    const [chanCount, setChanCount] = useState(0);
     const [hdlCount, setHdlCount] = useState(0);
 
     useEffect(() => {
         listen<JoinHandleStoreStatsPayload>('join_handle_store_stats', (event) => {
-            setChanCount(event.payload.channels);
             setHdlCount(event.payload.handles);
         })
     }, []);
 
     return (
         <>
-            <div>Channels: {chanCount}</div>
             <div>Handles: {hdlCount}</div>
         </>
     );
