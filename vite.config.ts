ee4c39cc6df238da15f21ef855d0cec6f456d06a
Christian Danscheid <webD97@users.noreply.github.com>
2024-11-25 08:21:54 +0100

    build: add manual rollup chunk for xterm

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,13 @@ export default defineConfig({
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          xterm: ['@xterm/xterm', '@xterm/addon-clipboard', '@xterm/addon-fit', '@xterm/addon-webgl']
+        }
+      }
+    }
   },
   plugins: [react()],
 })
