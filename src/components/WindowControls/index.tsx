f51ed19419570298cc272ae6ddbffebb86ee4577
Christian Danscheid <webD97@users.noreply.github.com>
2025-03-17 16:54:56 +0100

    fix(frontend): make trigger area of window controls larger so that they are clickable when the cursor is right at the edge

--- src/components/WindowControls/index.tsx
+++ src/components/WindowControls/index.tsx
@@ -11,17 +11,23 @@ export const WindowControls: React.FC = () => {
 
     return (
         <div className={classes.container}>
-            <div className={`${classes.button} ${classes.minimize}`} onClick={minimize}>
-                <svg className={classes.icon} viewBox="0 0 12 12"><rect y="6" width="12" height="2" /></svg>
+            <div className={classes.buttonWrapper} onClick={minimize}>
+                <div className={`${classes.button} ${classes.minimize}`}>
+                    <svg className={classes.icon} viewBox="0 0 12 12"><rect y="6" width="12" height="2" /></svg>
+                </div>
             </div>
-            <div className={`${classes.button} ${classes.maximize}`} onClick={maximize}>
-                <svg className={classes.icon} viewBox="0 0 12 12"><rect x="2" y="2" width="8" height="8" /></svg>
+            <div className={classes.buttonWrapper} onClick={maximize}>
+                <div className={`${classes.button} ${classes.maximize}`}>
+                    <svg className={classes.icon} viewBox="0 0 12 12"><rect x="2" y="2" width="8" height="8" /></svg>
+                </div>
             </div>
-            <div className={`${classes.button} ${classes.close}`} onClick={close}>
-                <svg className={classes.icon} viewBox="0 0 12 12">
-                    <line x1="2" y1="2" x2="10" y2="10" stroke="white" strokeWidth="2" />
-                    <line x1="10" y1="2" x2="2" y2="10" stroke="white" strokeWidth="2" />
-                </svg>
+            <div className={classes.buttonWrapper} onClick={close}>
+                <div className={`${classes.button} ${classes.close}`}>
+                    <svg className={classes.icon} viewBox="0 0 12 12">
+                        <line x1="2" y1="2" x2="10" y2="10" stroke="white" strokeWidth="2" />
+                        <line x1="10" y1="2" x2="2" y2="10" stroke="white" strokeWidth="2" />
+                    </svg>
+                </div>
             </div>
         </div>
     );
