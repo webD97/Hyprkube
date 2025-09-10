c227f8bc387b94beb933e9db1156bb8f05b6c49a
Christian Danscheid <webD97@users.noreply.github.com>
2025-09-10 07:34:00 +0200

    chore: fixups after dependency upgrades

--- src-tauri/src/frontend_commands/kube_stream_podlogs.rs
+++ src-tauri/src/frontend_commands/kube_stream_podlogs.rs
@@ -34,48 +34,54 @@ pub async fn kube_stream_podlogs(
     channel: tauri::ipc::Channel<LogStreamEvent>,
 ) -> Result<(), BackendError> {
     let client = client_registry_arc.try_clone(&client_id)?;
-
-    let pods: kube::Api<Pod> = kube::Api::namespaced(client, namespace);
-
-    let log_params = kube::api::LogParams {
-        follow: true,
-        tail_lines: Some(1000),
-        timestamps: false,
-        container: Some(container.into()),
-        ..Default::default()
-    };
-
-    let logs = pods.log_stream(name, &log_params).await?;
-
-    let log_stream = logs.compat();
-    let mut reader = BufReader::new(log_stream).lines();
-
+    let namespace = namespace.to_string();
+    let name = name.to_string();
+    let container = container.to_string();
     let channel_id = channel.id();
+
     info!("kube_stream_podlogs: channel {namespace}/{name} to {channel_id}");
 
     let stream_task = async move {
-        loop {
-            match reader.next_line().await {
-                Ok(Some(mut line)) => {
-                    line.push('\n');
-                    channel
-                        .send(LogStreamEvent::NewLine { lines: vec![line] })
-                        .unwrap();
-                }
-                Ok(None) => {
-                    channel.send(LogStreamEvent::EndOfStream {}).unwrap();
-                    break;
-                }
-                Err(error) => {
-                    channel
-                        .send(LogStreamEvent::Error {
-                            msg: error.to_string(),
-                        })
-                        .unwrap();
-                    break;
+        let pods: kube::Api<Pod> = kube::Api::namespaced(client, &namespace);
+
+        let log_params = kube::api::LogParams {
+            follow: true,
+            tail_lines: Some(1000),
+            timestamps: false,
+            container: Some(container),
+            ..Default::default()
+        };
+
+        match pods.log_stream(&name, &log_params).await {
+            Ok(logs) => {
+                let log_stream = logs.compat();
+                let mut reader = BufReader::new(log_stream).lines();
+
+                loop {
+                    match reader.next_line().await {
+                        Ok(Some(mut line)) => {
+                            line.push('\n');
+                            let _ = channel.send(LogStreamEvent::NewLine { lines: vec![line] });
+                        }
+                        Ok(None) => {
+                            let _ = channel.send(LogStreamEvent::EndOfStream {});
+                            break;
+                        }
+                        Err(error) => {
+                            let _ = channel.send(LogStreamEvent::Error {
+                                msg: error.to_string(),
+                            });
+                            break;
+                        }
+                    }
                 }
             }
-        }
+            Err(err) => {
+                let _ = channel.send(LogStreamEvent::Error {
+                    msg: err.to_string(),
+                });
+            }
+        };
     };
 
     join_handle_store.submit(channel_id, stream_task)?;
