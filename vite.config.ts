2478cc6f9d6ab3015b41cd393039a847e6796ddf
Christian Danscheid <webD97@users.noreply.github.com>
2025-10-16 17:06:07 +0200

    refactor: align configs with current Tauri defaults

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vite.dev/config/
 export default defineConfig({
-  // prevent vite from obscuring rust errors
+  plugins: [react()],
+
   clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
   server: {
+    port: 1420,
     strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
   },
-  // to access the Tauri environment variables set by the CLI with information about the current target
-  envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
   build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
     rollupOptions: {
       output: {
         manualChunks: {
@@ -26,6 +32,5 @@ export default defineConfig({
         }
       }
     }
-  },
-  plugins: [react()],
-})
+  }
+});
