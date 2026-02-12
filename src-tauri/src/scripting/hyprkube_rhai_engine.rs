349ed976c9a5256ee49b9f27be8a41e30b144663
Christian Danscheid <webD97@users.noreply.github.com>
2026-02-12 13:27:25 +0100

    zwischending

--- src-tauri/src/scripting/hyprkube_rhai_engine.rs
+++ src-tauri/src/scripting/hyprkube_rhai_engine.rs
@@ -1,19 +1,32 @@
 use std::{
     collections::HashMap,
     path::PathBuf,
-    sync::{Arc, Mutex},
+    sync::{Arc, Mutex, RwLock},
 };
 
 use kube::{
     api::{ApiResource, DynamicObject},
     Api, Discovery,
 };
-use rhai::{exported_module, CallFnOptions, EvalAltResult, FuncRegistration, Module};
+use rhai::{exported_module, CallFnOptions, Dynamic, EvalAltResult, FuncRegistration, Module};
 
 use crate::scripting::{modules, types::ResourceRef};
 
-pub struct HyprkubeRhaiEngine {
+struct ContextMenuSection<'a> {
+    pub matcher: rhai::FnPtr,
+    pub matcher_context: rhai::NativeCallContext<'a>,
+
+    pub items: rhai::FnPtr,
+    pub items_context: rhai::NativeCallContext<'a>,
+}
+
+pub struct ScriptingFacade<'a> {
     engine: rhai::Engine,
+
+    /// Handles scripts that render a resource context menu
+    resource_contextmenu_engine: rhai::Engine,
+    registered_contextmenu_sections: RwLock<HashMap<String, ContextMenuSection<'a>>>,
+
     resource_action_scripts: Mutex<HashMap<PathBuf, UserScript>>,
 }
 
@@ -22,16 +35,12 @@ pub struct UserScript {
 }
 
 #[allow(dead_code)]
-impl HyprkubeRhaiEngine {
+impl<'a> ScriptingFacade<'a> {
     pub async fn new(client: kube::Client, discovery: Arc<kube::Discovery>) -> Self {
-        let mut engine = rhai::Engine::new();
-
-        engine.build_type::<ResourceRef>();
-        engine.register_static_module("kube", Self::make_kube_module(client, discovery).into());
-        engine.register_static_module("base64", exported_module!(modules::base64_rhai).into());
-
         Self {
-            engine,
+            engine: rhai::Engine::new(),
+            resource_contextmenu_engine: Self::make_resource_contextmenu_engine(client, discovery),
+            registered_contextmenu_sections: RwLock::new(HashMap::new()),
             resource_action_scripts: Mutex::new(HashMap::new()),
         }
     }
@@ -48,6 +57,51 @@ impl HyprkubeRhaiEngine {
             .ok_or(format!("Kind not found: {}", kind))?;
 
         Ok(ar)
+    }
+
+    fn make_resource_contextmenu_engine(
+        client: kube::Client,
+        discovery: Arc<kube::Discovery>,
+    ) -> rhai::Engine {
+        let mut engine = rhai::Engine::new();
+
+        engine.build_type::<ResourceRef>();
+        engine.register_static_module("kube", Self::make_kube_module(client, discovery).into());
+        engine.register_static_module("base64", exported_module!(modules::base64_rhai).into());
+
+        engine
+    }
+
+    /// Runs all scripts that contribute to the given resource's context menu and returns the specification.
+    #[allow(unused)]
+    pub fn evaluate_resource_contextmenu(&self, obj: kube::api::DynamicObject) {
+        let api_version = obj.types.as_ref().unwrap().api_version.to_owned();
+        let kind = obj.types.as_ref().unwrap().kind.to_owned();
+        let obj = rhai::serde::to_dynamic(obj).unwrap();
+
+        let sections_defs = self.registered_contextmenu_sections.read().unwrap();
+
+        let mut sections: Vec<Vec<Dynamic>> = Vec::new();
+
+        for section_def in sections_defs.values() {
+            let matches = section_def
+                .matcher
+                .call_within_context::<bool>(&section_def.matcher_context, ("", "", kind.clone()))
+                .unwrap();
+
+            if !matches {
+                continue;
+            }
+
+            let section_items = section_def
+                .items
+                .call_within_context::<rhai::Array>(&section_def.items_context, (obj.clone(),))
+                .unwrap();
+
+            sections.push(section_items);
+        }
+
+        todo!();
     }
 
     fn make_kube_module(client: kube::Client, discovery: Arc<kube::Discovery>) -> Module {
@@ -171,7 +225,7 @@ mod tests {
             .await
             .unwrap();
 
-        let engine = HyprkubeRhaiEngine::new(client, Arc::new(discovery)).await;
+        let engine = ScriptingFacade::new(client, Arc::new(discovery)).await;
 
         let script: PathBuf =
             "/home/christian/Repositories/github.com/webd97/_sandbox/kube-rhai/src/script.rhai"
