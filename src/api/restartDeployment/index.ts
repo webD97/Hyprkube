76039abd731b6f35f0539c3c1d863005a443233b
Christian Danscheid <webD97@users.noreply.github.com>
2026-03-07 16:13:03 +0100

    refactor: remove unused commands

--- src/api/restartDeployment/index.ts
+++
@@ -1,9 +0,0 @@
-import { invoke } from "@tauri-apps/api/core";
-import { KubeContextSource } from "../../hooks/useContextDiscovery";
-
-export default function restartDeployment(contextSource: KubeContextSource, namespace: string, name: string) {
-    return invoke<void>('restart_deployment', {
-        contextSource,
-        namespace, name
-    });
-}
