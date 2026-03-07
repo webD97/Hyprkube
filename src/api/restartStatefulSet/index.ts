378713f563af9b5eb4407134c7d127418f8e4c71
Christian Danscheid <webD97@users.noreply.github.com>
2026-03-07 16:13:03 +0100

    refactor: remove unused commands

--- src/api/restartStatefulSet/index.ts
+++
@@ -1,9 +0,0 @@
-import { invoke } from "@tauri-apps/api/core";
-import { KubeContextSource } from "../../hooks/useContextDiscovery";
-
-export default function restartStatefulSet(contextSource: KubeContextSource, namespace: string, name: string) {
-    return invoke<void>('restart_statefulset', {
-        contextSource,
-        namespace, name
-    });
-}
