c37ff23fca8b6165894d7c1ac4f74c92ba05722a
Christian Danscheid <webD97@users.noreply.github.com>
2026-03-07 12:35:28 +0100

    refactor: re-organize how extensions are organized and add rough docs in README

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Please note that this project is still far away from an MVP-status and is not su
 by [Rhai](https://rhai.rs/) scripts.
 - **Pinned Kinds** allow you to focus on the resources you care about - no more scrolling through
 an endless list of CRDs
+- **Extensions** allow you to extend the functionality of Hyprkube in a safe and controlled manner.
 
 ## Development
 Hyprkube is built on [Tauri](https://tauri.app) and [React](https://react.dev).
@@ -19,3 +20,36 @@ development environment, including installing Node.js and Rust.
 ## Building
 Hyprkube can be built by running `npm run tauri build`. The build process currently produces deb and rpm packages.
 Other platform might follow at some point.
+
+## Extensions
+Hyprkube can be extended to integrate with your daily workflows by creating extensions. These extensions can be used to
+extend the context menus of Kubernetes resources with custom actions. Extensions can be placed in:
+
+- Linux: `~/.local/share/de.webd97.hyprkube/extensions`
+- macOS: `~/Library/Application Support/de.webd97.hyprkube/extensions`
+- Windows: `%APPDATA%\Roaming\de.webd97.hyprkube\extensions`
+
+Each extension is a directory with the following optional subdirectories - depending on the functionality that you want
+to add. The capabilities of each script are determined by the subdirectory they live in. At runtime, these are mapped to
+specialized scripting engines with exactly the features that are permitted. This ensures both fast execution and also
+prevents undesirable side effects if a scripts tries to do things that it should not do.
+
+```
+~/.local/share/de.webd97.hyprkube/extensions
+├── cert-manager
+│   └── menus
+│       ├── 000-my-shortcuts.rhai
+│       └── 100-extras.rhai
+└── fleet
+    └── menus
+        └── 000-gitrepo.rhai
+```
+
+### Custom resource context menus
+Scripts in the `menus` subdirectory can be used to extend the context menus of Kubernetes resources. The following
+Rhai modules are available:
+
+- `base64`
+- `kube`
+- `frontend`
+- `clipboard`
--- src-tauri/src/scripting/scripts_provider.rs
+++ src-tauri/src/scripting/scripts_provider.rs
@@ -3,7 +3,11 @@ use std::{io::ErrorKind, path::PathBuf};
 use scan_dir::ScanDir;
 use tauri::Manager as _;
 
-const SCRIPT_ENTRYPOINT_FILE: &str = "main.rhai";
+/// A ScriptType determines which capabilties are available in the scripting engine
+/// at runtime. This avoids unexpected side-effects of poorly-written scripts.
+pub enum ScriptType {
+    Menu,
+}
 
 pub struct ScriptsProvider {
     app: tauri::AppHandle,
@@ -14,53 +18,15 @@ impl ScriptsProvider {
         ScriptsProvider { app }
     }
 
-    /// Returns a list of paths to entrypoints of builtin (i.e. bundled with the application) scripts. Bundled scripts are not
-    /// expected to be changed at runtime.
-    ///
-    /// Refer to [`tauri::path::PathResolver::resource_dir`] to understand how paths are resolved during development and at runtime.
-    pub fn get_builtins_entrypoints(&self) -> Result<Vec<PathBuf>, Error> {
-        let builtins_base = self
-            .app
-            .path()
-            .resource_dir()
-            .map(|mut dir| {
-                dir.push("scripts");
-                dir.push("builtin");
-                dir
-            })
-            .map_err(Error::TauriDir)?;
+    fn list_packages_in(&self, dir: &PathBuf) -> Vec<PathBuf> {
+        let mut packages: Vec<PathBuf> = Vec::new();
 
-        let mut entrypoint = builtins_base.clone();
-        entrypoint.push(SCRIPT_ENTRYPOINT_FILE);
-
-        Ok(Self::sanitize_nonexistent(vec![entrypoint]))
-    }
-
-    /// Returns a list of paths to entrypoints of user-provided scripts. These scripts might even be changed at runtime but there
-    /// is neither a watching nor a reload mechanism yet.
-    pub fn get_extensions_entrypoints(&self) -> Result<Vec<PathBuf>, Error> {
-        let extensions_base = self
-            .app
-            .path()
-            .app_data_dir()
-            .map(|mut dir| {
-                dir.push("extensions");
-                dir
-            })
-            .map_err(Error::TauriDir)?;
-
-        let mut extension_entrypoints: Vec<PathBuf> = Vec::new();
-
-        let result = ScanDir::dirs().walk(&extensions_base, |iter| {
+        let result = ScanDir::dirs().walk(dir, |iter| {
             let mut entrypoints = iter
                 .map(|(entry, _)| entry.path())
-                .map(|mut path| {
-                    path.push(SCRIPT_ENTRYPOINT_FILE);
-                    path
-                })
                 .collect::<Vec<PathBuf>>();
 
-            extension_entrypoints.append(&mut entrypoints);
+            packages.append(&mut entrypoints);
         });
 
         if let Err(errors) = result {
@@ -74,30 +40,82 @@ impl ScriptsProvider {
             }
         }
 
-        Ok(Self::sanitize_nonexistent(extension_entrypoints))
+        packages
     }
 
-    /// Removes files that do not exist.
-    fn sanitize_nonexistent(mut files: Vec<PathBuf>) -> Vec<PathBuf> {
-        files.retain(|file| {
-            let exists = std::fs::exists(file).unwrap();
+    fn get_builtin_packages(&self) -> Result<Vec<PathBuf>, Error> {
+        let builtins_base = self.app.path().resource_dir().map(|mut dir| {
+            dir.push("scripts");
+            dir
+        })?;
 
-            if !exists {
-                tracing::warn!(
-                    "Entrypoint {} does not exist, ignoring.",
-                    file.to_string_lossy()
-                );
-            }
+        Ok(self.list_packages_in(&builtins_base))
+    }
 
-            exists
+    fn get_extension_packages(&self) -> Result<Vec<PathBuf>, Error> {
+        let extensions_base = self
+            .app
+            .path()
+            .app_data_dir()
+            .map(|mut dir| {
+                dir.push("extensions");
+                dir
+            })
+            .map_err(Error::TauriDir)?;
+
+        Ok(self.list_packages_in(&extensions_base))
+    }
+
+    fn get_scripts_in_package(
+        &self,
+        mut package_path: PathBuf,
+        script_type: &ScriptType,
+    ) -> Vec<PathBuf> {
+        let mut scripts: Vec<PathBuf> = Vec::new();
+
+        match script_type {
+            ScriptType::Menu => package_path.push("menus"),
+        }
+
+        let result = ScanDir::files().walk(package_path, |iter| {
+            let mut entrypoints = iter
+                .filter(|(_, name)| name.ends_with(".rhai"))
+                .map(|(ref entry, _)| entry.path())
+                .collect::<Vec<PathBuf>>();
+
+            scripts.append(&mut entrypoints);
         });
 
-        files
+        if let Err(errors) = result {
+            let errors = errors.into_iter().filter(|e| match e {
+                scan_dir::Error::Io(e, _) => e.kind() != ErrorKind::NotFound,
+                scan_dir::Error::Decode(_) => true,
+            });
+
+            for e in errors {
+                tracing::warn!("{e}");
+            }
+        }
+
+        scripts.sort();
+
+        scripts
+    }
+
+    pub fn get_scripts_for_type(&self, script_type: ScriptType) -> Result<Vec<PathBuf>, Error> {
+        let builtins = self.get_builtin_packages()?;
+        let extensions = self.get_extension_packages()?;
+
+        Ok(builtins
+            .into_iter()
+            .chain(extensions)
+            .flat_map(|pkg| self.get_scripts_in_package(pkg, &script_type))
+            .collect())
     }
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("Error resolving directory: {0}")]
-    TauriDir(tauri::Error),
+    TauriDir(#[from] tauri::Error),
 }
